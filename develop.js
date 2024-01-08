49d1b10cc64bf0a4ae5c46491dd52aa90c563577
Yluzna <anzulygarcia@gmail.com>
2024-01-08 09:41:45 +0100

    add 4 elements

--- develop.js
+++ develop.js
@@ -28,8 +28,8 @@ list.addEventListener('click', function (ev) {
   }
 }, false);
 
-// Crear un nuevo elemento de la lista al hacer clic en el botón "Añadir"
-function newElement() {
+// Crear un nuevo elemento Vet de la lista al hacer clic en el botón "Añadir"
+function newElementVet() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("vet").value;
   var t = document.createTextNode(inputValue);
@@ -37,9 +37,86 @@ function newElement() {
   if (inputValue === '') {
     alert("¡Add Text!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("myVet").appendChild(li);
   }
   document.getElementById("vet").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Crear un nuevo elemento Walks de la lista al hacer clic en el botón "Añadir"
+function newElementWalks() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("walks").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("¡Add Text!");
+  } else {
+    document.getElementById("myWalks").appendChild(li);
+  }
+  document.getElementById("walks").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Crear un nuevo elemento Meals de la lista al hacer clic en el botón "Añadir"
+function newElementMeals() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("meals").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("¡Add Text!");
+  } else {
+    document.getElementById("myMeals").appendChild(li);
+  }
+  document.getElementById("meals").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+// Crear un nuevo elemento showers de la lista al hacer clic en el botón "Añadir"
+function newElementShowers() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("showers").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("¡Add Text!");
+  } else {
+    document.getElementById("myShowers").appendChild(li);
+  }
+  document.getElementById("showers").value = "";
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
